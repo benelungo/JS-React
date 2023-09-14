326d6bde326623eb266ad095f7d06269fb4213ae
benelungo <143945424+benelungo@users.noreply.github.com>
2023-09-14 05:55:41 +0300

    structure changes, repeatable code replaces functions

--- js/script.js
+++ js/script.js
@@ -18,21 +18,20 @@ if (numberOfFilms) {
 }
 
 for (let i = 0; i < 2; i += 1) {
-    let watchedMovie;
-    while (true) {
-        watchedMovie = prompt('Which last movie do you watch: ', '');
-        if(watchedMovie && watchedMovie.length <= 50) break;
-        alert('Please enter a movie name!(50 characters max)');
-    }
-    let movieRating
-    while (watchedMovie) {
-        movieRating = +prompt('Movie rating: ', '');
-        if (movieRating) break;
-        alert('Please enter a movie rating in digits!')
-    }
+    let watchedMovie = askQuestion('Which last movie do you watch: ', 'Please enter a movie name!(50 characters max)')
+    let movieRating = askQuestion('Movie rating: ', 'Please enter a movie rating in digits!(50 characters max)')
+
     personalMoviesDB.movies[watchedMovie] = movieRating;
 }
 
-
 console.log(personalMoviesDB);
 
+function askQuestion(question, warning){
+    let answer
+    while (true) {
+        answer = prompt(question, '');
+        if(answer && answer.length <= 50) break;
+        alert(warning);
+    }
+    return answer;
+}
