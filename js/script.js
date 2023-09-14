a478c7c1dcdea33fa0d27a059d608f587cadc0f5
benelungo <143945424+benelungo@users.noreply.github.com>
2023-09-14 03:36:59 +0300

    add questions about movies in promt

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
+const numberOfFilms = +prompt('Number of watched films: ', '');
+const personalMoviesDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false,
+};
 
-//const category = "toys";
-// console.log(`https://rozetka.com.ua/${category}`)
 
-let a = null;
-console.log(!a);
+for (let i = 0; i < 2; i += 1) {
+    let watchedMovie = prompt('Which last movie do you watch: ', '');
+    let movieRating = +prompt('Movie rating: ', '');
+
+    personalMoviesDB.movies[watchedMovie] = movieRating;
+
+    if (!movieRating) {
+      i--;
+    }
+}
+
+console.log(personalMoviesDB);
 
